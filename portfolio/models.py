fb3e27543ba4ce8ee42f8cede387b70b5a74dea9
Junior Celulares <eduardogiorello@hotmail.com>
2023-07-14 09:37:58 -0300

    g

--- portfolio/models.py
+++ portfolio/models.py
@@ -5,6 +5,13 @@ from django.db.models.fields.files import ImageField
 
 class Project(models.Model):
     title = CharField(max_length=100)
+    subtitulo = CharField(max_length=100, null=True)
     description = CharField(max_length=250)
+    cliente = CharField(max_length=100, null=True)
+    categoria = CharField(max_length=100, null=True)
     image = ImageField(upload_to='portfolio/images')
     url = URLField(blank=True)
+    
+
+    def __str__(self):
+        return 'Proyecto ' + str(self.title) + ' - Cliente - ' + str(self.cliente)
